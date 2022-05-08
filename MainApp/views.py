b315bd32cbdbd20cba4eb4c7b2e8ab9e88548a0f
DavidK00 <Dav3dkoo@gmail.com>
2022-05-07 20:35:15 -0500

    final

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,26 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.template import loader
-from .models import Pizza, Toppings
+from .models import Pizza
+from .forms import CommentForm
 
 
 # Create your views here.
 def index(request):
     return render(request, 'MainApp/index.html')
 
-def pizza(request):
+def pizzas(request):
     pizzas = Pizza.objects.all()
-    template = loader.get_template('MainApp/list.html')
     context = {'pizzas':pizzas}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'MainApp/pizzas.html', context)
 
-def indiv_pizza(request, pizza_id):
-    indiv_pizza = Pizza.objects.get(id = pizza_id)
-    toppings = Toppings.toppings_set()
-    template = loader.get_template('MainApp/pizza.html')
-    context = {'indiv_pizza':indiv_pizza, "toppings": toppings}
-    return HttpResponse(template.render(context, request))
-
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.toppings_set.all()
+    context = {'pizza':pizza, "toppings": toppings}
+    return render(request, 'MainApp/pizza.html', context)
+def new(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else: 
+        form = CommentForm()
+    context = {'form', form}
+    return render(request, 'MainApp/pizza.html', context)
 
 
 
